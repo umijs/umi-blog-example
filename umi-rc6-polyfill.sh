1fd0061f28b9a36a3768557da4390b668bd3d98f
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-03-18 12:08:43 +0800

    Remove polyfill for rc6

--- umi-rc6-polyfill.sh
+++
@@ -1,3 +0,0 @@
-awk 'NR==26{print "platform: \"node\","}1' node_modules/@umijs/preset-umi/dist/features/apiRoute/dev-server/esbuild.js > node_modules/@umijs/preset-umi/dist/features/apiRoute/dev-server/esbuild.new.js
-rm node_modules/@umijs/preset-umi/dist/features/apiRoute/dev-server/esbuild.js
-mv node_modules/@umijs/preset-umi/dist/features/apiRoute/dev-server/esbuild.new.js node_modules/@umijs/preset-umi/dist/features/apiRoute/dev-server/esbuild.js
