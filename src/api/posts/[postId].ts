afe11553ade66d2605b3d9d16d4fa2d34225a32e
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-03-21 16:44:15 +0800

    Add Redis cache for post data

--- src/api/posts/[postId].ts
+++ src/api/posts/[postId].ts
@@ -1,21 +1,31 @@
 import { UmiApiRequest, UmiApiResponse } from "umi";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import { Redis } from "@upstash/redis";
 
 export default async function (req: UmiApiRequest, res: UmiApiResponse) {
   let prisma: PrismaClient;
   switch (req.method) {
     case 'GET':
-      prisma = new PrismaClient();
-      const post = await prisma.post.findUnique({
-        where: { id: +req.params.postId },
-        include: { author: true }
-      });
-      if (!post) {
-        res.status(404).json({ error: 'Post not found.' });
+      const redis = Redis.fromEnv();
+      let post = await redis.get('post-' + req.params.postId);
+      if (post) {
+        res.status(200).json(post);
         return;
       }
-      res.status(200).json(post);
-      await prisma.$disconnect()
+      if (!post) {
+        prisma = new PrismaClient();
+        post = await prisma.post.findUnique({
+          where: { id: +req.params.postId },
+          include: { author: true }
+        });
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({ error: 'Post not found.' });
+        }
+        await redis.set('post-' + req.params.postId, JSON.stringify(post));
+        await prisma.$disconnect();
+      }
       break;
     default:
       res.status(405).json({ error: 'Method not allowed' })
